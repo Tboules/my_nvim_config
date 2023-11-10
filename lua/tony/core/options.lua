caa0686a29cf50df4802ecb258b1e49d8a0aa862
Tboules <tboules@gmail.com>
2023-11-09 20:31:45 -0800

    my-initial-config

---
+++ lua/tony/core/options.lua
@@ -0,0 +1,27 @@
+vim.o.relativenumber = true
+vim.o.number = true
+
+vim.o.mouse = 'a'
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+
+vim.opt.termguicolors = true
+vim.background = "dark"
+
+vim.o.undofile = true
+vim.o.hlsearch = false
+vim.o.breakindent = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.completeopt = 'menuone,noselect'
+
+vim.o.wrap = false
+
+vim.o.signcolumn = "yes"
---
+++ lua/tony/plugins/theme.lua
@@ -0,0 +1,11 @@
+return { 
+ "bluz71/vim-moonfly-colors",
+  name = "moonfly",
+  lazy = false,
+  priority = 1000,
+
+  config = function()
+    vim.cmd [[colorscheme moonfly]]
+    vim.g.moonflyTransparent = true
+  end
+}
