b71683b007bdf956b0381cfc62816932dc68d0d7
Tboules <tboules@gmail.com>
2024-01-03 20:58:09 -0800

    more cool remaps

--- lua/tony/core/keymaps.lua
+++ lua/tony/core/keymaps.lua
@@ -1,36 +1,40 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local keymap = vim.keymap
+local map = vim.keymap.set
 local defaults = { noremap = true, silent = true }
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+map("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- vim commands
-keymap.set("n", "<leader>pv", vim.cmd.Ex)
-keymap.set("i", "<C-l>", "<esc>la")
+map("n", "<leader>pv", vim.cmd.Ex)
+map("i", "<C-l>", "<esc>la")
 
-keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "save file" })
+map("n", "<leader>w", "<cmd>w<cr>", { desc = "save file" })
 
-keymap.set("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-keymap.set("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-keymap.set("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-keymap.set("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
-keymap.set("n", "<S-Down>", ":m .+1<CR>==", defaults)
-keymap.set("n", "<S-Up>", ":m .-2<CR>==", defaults)
+map("n", "<S-Down>", ":m .+1<CR>==", defaults)
+map("n", "<S-Up>", ":m .-2<CR>==", defaults)
 
-keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", defaults)
-keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", defaults)
+map("v", "<S-Down>", ":m '>+1<CR>gv=gv", defaults)
+map("v", "<S-Up>", ":m '<-2<CR>gv=gv", defaults)
+
+-- Insert empty line without entering insert mode
+map("n", "<leader>o", ':<C-u>call append(line("."), repeat([""], v:count1))<CR>', defaults)
+map("n", "<leader>O", ':<C-u>call append(line(".")-1, repeat([""], v:count1))<CR>', defaults)
